7e30057b5ac6e64aae7e91cc5e1d4a19c2da1160
misquishy3244-hub <misquishy3244@gmail.com>
2026-08-18 16:51:28 -0500

    Update ASAP.py

--- CLEAR/ACPv1/src/ACPv1/ASAP.py
+++ CLEAR/ACPv1/src/ACPv1/ASAP.py
@@ -55,24 +55,34 @@ async def _run(LeftMotorList: list[Motor], RightMotorList: list[Motor], GearRati
 
             RequestedRightRPM= RightPos*(MotorRpmMax/100)
             RequestedLeftRPM= LeftPos*(MotorRpmMax/100)
+            AcutalRightRPM= (RightMotorList[0].velocity(RPM) + RightMotorList[1].velocity(RPM))/2
+            AcutalLeftRPM= (LeftMotorList[0].velocity(RPM) + LeftMotorList[1].velocity(RPM))/2
+
+            VelocityDiffrenceRight=RightMotorList[0].velocity(RPM) - RightMotorList[1].velocity(RPM)
+            VelocityDiffrenceLeft=LeftMotorList[0].velocity(RPM) - LeftMotorList[1].velocity(RPM)
 
             LeftWheelSpeed=((LeftMotorList[0].velocity(RPM)*((2*3.14159)/60))/GearRatio)*(WheelSize_MM/1000)
             RightWheelSpeed=((RightMotorList[0].velocity(RPM)*((2*3.14159)/60))/GearRatio)*(WheelSize_MM/1000)
-            TrueSpeed=XOdom.velocity(RPM)*((2*3.14159)/60)*(OdomWheelSize_MM/1000)
 
-            SlipRateRight=((TrueSpeed-RightWheelSpeed)/TrueSpeed)*100
-            SlipRateLeft=((TrueSpeed-LeftWheelSpeed)/TrueSpeed)*100
+            TrueSpeedFilter=LKF.LinearKalmanFilter(A=LKF.Matrix([[1]]), B=LKF.Matrix([[0.02]]), H=LKF.Matrix([[1/(WheelSize_MM/1000)]]), Q=LKF.Matrix([[0.05]]), R=LKF.Matrix([[0.25]]), x0=LKF.Matrix([[0]]), P0=LKF.Matrix([[9]]))
+            TrueSpeedFilter.predict(LKF.Matrix([[Inertial.acceleration(XAXIS)*9.81]]))
+            TrueSpeedFilter.update(LKF.Matrix([[XOdom.velocity(RPM)]]))
 
-            SpeedOffsetLeft=min(SlipRateLeft-100, 0)
-            SpeedOffsetRight=min(SlipRateRight-100, 0)
+            TrueSpeed= TrueSpeedFilter.x.data[0][0]
 
-            ControllerMultipyer=MotorRpmMax/100
+            SlipRateRight=(TrueSpeed-RightWheelSpeed)/TrueSpeed*100
+            SlipRateLeft=(TrueSpeed-LeftWheelSpeed)/TrueSpeed*100
 
-            for motor in LeftMotorList:
-                motor.spin(FORWARD, (RequestedLeftRPM-SpeedOffsetLeft)*ControllerMultipyer, RPM)
-            
-            for motor in RightMotorList:
-                motor.spin(FORWARD, (RequestedRightRPM-SpeedOffsetRight)*ControllerMultipyer, RPM)
-            
+            TargetRightRPM=RequestedRightRPM-SlipRateRight
+            TargetLeftRPM=RequestedLeftRPM-SlipRateLeft
+            AntiFightTargetRightRPM=[TargetRightRPM-((VelocityDiffrenceRight/2)*0.5), TargetRightRPM+((VelocityDiffrenceRight/2)*0.5)]
+            AntiFightTargetLeftRPM=[TargetLeftRPM-((VelocityDiffrenceLeft/2)*0.5), TargetLeftRPM+((VelocityDiffrenceLeft/2)*0.5)]
+
+            for i in range(len(LeftMotorList)):
+                LeftMotorList[i].spin(FORWARD, PDC.PD("LeftSide", 0.5, 0.1).compute(AntiFightTargetLeftRPM[i], AcutalLeftRPM, 0.02, 12, -12), VOLT)
+
+            for i in range(len(RightMotorList)):
+                RightMotorList[i].spin(FORWARD, PDC.PD("RightSide", 0.5, 0.1).compute(AntiFightTargetRightRPM[i], AcutalRightRPM, 0.02, 12, -12), VOLT)
+
             await uasyncio.sleep_ms(20 - StartTime)
 
